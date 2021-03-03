04251a7934c4023f1973b579254f5ef2a2153481
Borja Muñoz <bmunoz@carto.com>
2021-03-03 14:14:56 +0100

    Reorder imports

--- deck.gl/examples/pure-js/angular/src/app/modules/home/home.module.ts
+++ deck.gl/examples/pure-js/angular/src/app/modules/home/home.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from "./home.component";
-import { SharedModule } from "../shared/shared.module";
-import { SidebarComponent } from './views/sidebar/sidebar.component';
-import { HomeRoutingModule } from "./home-routing.module";
-import { ChartComponent } from "./components/chart/chart.component";
-import { NgxEchartsModule } from "ngx-echarts";
-import * as echarts from 'echarts';
-import { RailRoadsLayer } from './layers/rail-roads-layer';
-import { BuildingsLayer } from './layers/buildings-layer';
-import { StoresLayer } from './layers/stores-layer';
-import { ToggleComponent } from './components/toggle/toggle.component';
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatDividerModule } from "@angular/material/divider";
+
+import { NgxEchartsModule } from "ngx-echarts";
+import * as echarts from 'echarts';
+
+import { HomeRoutingModule } from "./home-routing.module";
+import { SharedModule } from "../shared/shared.module";
+import { HomeComponent } from "./home.component";
+import { SidebarComponent } from './views/sidebar/sidebar.component';
+import { ChartComponent } from "./components/chart/chart.component";
+import { ToggleComponent } from './components/toggle/toggle.component';
+import { RailRoadsLayer } from './layers/rail-roads-layer';
+import { BuildingsLayer } from './layers/buildings-layer';
+import { StoresLayer } from './layers/stores-layer';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,6 @@ import { MatDividerModule } from "@angular/material/divider";
     MatGridListModule,
     MatDividerModule
   ],
-  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeModule {
 }
