3472be456e9beb45e0b4e3a43394a1f2078933bd
Borja Muñoz <bmunoz@carto.com>
2021-03-03 14:19:24 +0100

    Reorganize imports

--- deck.gl/examples/pure-js/angular/src/app/app-routing.module.ts
+++ deck.gl/examples/pure-js/angular/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './modules/home/home.component';
 import { SidebarComponent } from "./modules/home/views/sidebar/sidebar.component";
 
