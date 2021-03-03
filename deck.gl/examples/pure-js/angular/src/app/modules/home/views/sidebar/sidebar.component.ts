546bf2e261e75ffa7985942a73ce21cb8b195ecc
Borja Muñoz <bmunoz@carto.com>
2021-03-03 14:09:03 +0100

    Remove suscription to viewportLoaded layer event

--- deck.gl/examples/pure-js/angular/src/app/modules/home/views/sidebar/sidebar.component.ts
+++ deck.gl/examples/pure-js/angular/src/app/modules/home/views/sidebar/sidebar.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+
 import { RailRoadsLayer } from "../../layers/rail-roads-layer";
 import { BuildingsLayer } from "../../layers/buildings-layer";
 import { StoresLayer } from "../../layers/stores-layer";
 import { MapService } from "../../../../services/map.service";
-import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-sidebar',
@@ -11,8 +11,6 @@ import { Subscription } from "rxjs";
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit, OnDestroy {
-
-  subscription: Subscription;
 
   constructor(
     private railRoadsLayer: RailRoadsLayer,
@@ -24,29 +22,11 @@ export class SidebarComponent implements OnInit, OnDestroy {
 
   async ngOnInit() {
     await this.mapService.addLayer(this.railRoadsLayer);
-
-    /* this.subscription = this.railRoadsLayer.viewportLoaded.subscribe(data => {
-      console.log(`Data from ${this.railRoadsLayer.id} has arrived!`, data);
-    }); */
-
     await this.mapService.addLayer(this.buildingsLayer);
-
-    /* this.subscription = this.buildingsLayer.viewportLoaded.subscribe(data => {
-      console.log(`Data from ${this.buildingsLayer.id} has arrived!`, data);
-    }); */
-
     await this.mapService.addLayer(this.storesLayer);
-
-    /* this.subscription = this.storesLayer.viewportLoaded.subscribe(data => {
-      console.log(`Data from ${this.storesLayer.id} has arrived!`, data);
-    }); */
   }
 
   ngOnDestroy () {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-
     this.mapService.removeLayer(this.railRoadsLayer.id);
     this.mapService.removeLayer(this.buildingsLayer.id);
     this.mapService.removeLayer(this.storesLayer.id);
