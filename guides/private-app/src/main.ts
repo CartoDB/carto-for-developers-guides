7f2c646b8129f84eb6f068b726e11e93c68c592d
Verónica Milán <vmilan@cartodb.com>
2023-05-30 16:35:28 +0200

    UI adaptations

--- guides/private-app/src/main.ts
+++ guides/private-app/src/main.ts
@@ -1,20 +1,7 @@
-import './style.css'
 import { initAuth } from './auth'
 import { createMap } from './map'
 import { setDefaultCredentials }from '@deck.gl/carto/typed'
 import { getAirports } from './api'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <button id="login">Login</button>
-  <div id="login-content">
-    <header>
-      <button id="logout">Logout</button>
-      <div id="profile"></div>
-    </header>
-    <div id="map"></div>
-    <canvas id="deck-canvas"></canvas>
-  </div>
-`
 
 initAuth().then((accessToken) => {
   if (accessToken) {
