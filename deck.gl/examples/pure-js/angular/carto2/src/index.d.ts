33c48d979c158dfc294dfe7ebd48e275a64f99c0
Alberto Asuero <albertoasuero@gmail.com>
2023-10-31 18:18:47 +0100

    Remove old doc and revamp repo to guides

--- deck.gl/examples/pure-js/angular/carto2/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@deck.gl/core';
-declare module '@deck.gl/carto';
-declare module '@deck.gl/layers';
--- deck.gl/examples/pure-js/angular/carto2/src/utils/formatter.ts
+++
@@ -1,9 +0,0 @@
-export function numberFormatter(value: number) {
-  return Intl.NumberFormat('en-US', {
-    maximumFractionDigits: 1,
-    minimumFractionDigits: 0,
-    // @ts-ignore
-    notation: 'compact',
-    compactDisplay: 'short',
-  }).format(value);
-};
